b99947b4b0bf5acd6cecbc0f209a2fed3bf326ed
lanzu ln <lanzu.dev@gmail.com>
2023-02-18 02:39:06 +0600

    apply loop on a simple array

--- array.php
+++ array.php
@@ -2,3 +2,25 @@
 
 $country = ["Bangladesh", "pakistan", "india", "nepal"];
 echo $country[0];
+echo PHP_EOL;
+echo "==============";
+echo PHP_EOL;
+
+//echo count($country[0]);____________count use for find the length;
+
+// apply loop on simple array
+$n = count($country); // function আগেই কল করে নিলে বার বার রান করা লাগে না, এতে করে run দ্রুত হবে।
+for ($i = 0; $i < $n; $i++) {
+    echo $country[$i];
+    echo PHP_EOL;
+}
+
+echo "==============";
+echo PHP_EOL;
+
+//উল্টাটা হবে, মানে শেষেরটা আগে আসবে।
+$n = count($country);
+for ($i = $n - 1; $i >= 0; $i--) {
+    echo $country[$i];
+    echo PHP_EOL;
+}
