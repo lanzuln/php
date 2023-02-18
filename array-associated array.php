c9622ad7cc50d89865fe914d91d45c82e8ad2ccc
lanzu ln <lanzu.dev@gmail.com>
2023-02-18 14:26:51 +0600

    associative array added

---
+++ array-associated array.php
@@ -0,0 +1,49 @@
+<?php
+
+$mobile = [
+    "samsung" => "s21, s22, s23",
+    "iphone"  => "11, 12, 13 max pro,14 max pro ",
+    "oneplus" => "8t, 9r, 9rt",
+];
+
+// echo count($mobile);
+// echo $mobile['samsung'];
+
+// ============== loop for associative array
+// foreach ($mobile as $key => $value) {
+//     echo $key . "=" . $value;
+//     echo PHP_EOL;
+// }
+
+/*
+// =============== show keys
+echo "see keys name:";
+echo PHP_EOL;
+
+$seeAll = array_keys($mobile);
+print_r($seeAll);
+echo PHP_EOL;
+
+// =============== show values
+echo "see values name:";
+echo PHP_EOL;
+$seeAll = array_values($mobile);
+print_r($seeAll);
+
+ */
+
+/*
+//  ===========apply loop on  key/value
+$seeAll = array_keys($mobile);
+for ($i = 0; $i < count($seeAll); $i++) {
+$key = $seeAll[$i];
+echo $key;
+echo PHP_EOL;
+}*/
+
+//====================যদি চাই array এর মাঝে আর value অ্যাড করতে , তাহলে
+$mobile['samsung'] = $mobile['samsung'] . ", A30";
+// ________আর সহজে লিখা যায়
+$mobile['samsung'] .= ", A20";
+
+print_r($mobile);
