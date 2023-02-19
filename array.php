7807a75f59bac80252ea5d617b37708232b794cc
lanzu ln <lanzu.dev@gmail.com>
2023-02-19 13:25:02 +0600

    simple modification on array.php

--- array.php
+++ array.php
@@ -2,6 +2,8 @@
 
 $country = ["Bangladesh", "pakistan", "india", "nepal"];
 echo $country[0];
+echo PHP_EOL; //একটা value দেখার জন্য;
+print_r($country); //see all keys and values on array
 echo PHP_EOL;
 echo "==============";
 echo PHP_EOL;
@@ -22,5 +24,13 @@ echo PHP_EOL;
 $n = count($country);
 for ($i = $n - 1; $i >= 0; $i--) {
     echo $country[$i];
+    echo PHP_EOL;
+}
+;
+// another system of apply loop on simple array
+echo "==============";
+echo PHP_EOL;
+foreach ($country as $eachCountry) {
+    echo $eachCountry;
     echo PHP_EOL;
 }
