a808e9aa46dcc0d108640da1ad1d3c84fe4eed85
lanzu ln <lanzu.dev@gmail.com>
2023-02-21 21:57:39 +0600

    add 11 number

--- m4v10-array-extraction.php
+++ m4v10-array-extraction.php
@@ -1,1 +1,27 @@
 <?php
+
+$mobile = ['samsung', 'iphone', 'oppo', 'vivo', 'xiomi', 'sony', 'lg'];
+print_r($mobile);
+
+$arraySlice = array_slice($mobile, 1, 3);
+print_r($arraySlice);
+$arraySlice = array_slice($mobile, 1, -1); // শেষের আগ পর্যন্ত।
+print_r($arraySlice);
+$arraySlice = array_slice($mobile, -4, 2);
+print_r($arraySlice);
+
+// যদি value গুলার key ঠিক রাখতে চাই, তাহলে true
+
+$arraySlice = array_slice($mobile, 1, 3, true);
+print_r($arraySlice);
+
+// ______associative array কে slice korte chyle
+$random = [
+    "a" => 41,
+    "b" => 53,
+    "c" => 2341,
+    "d" => 5651, 43,
+];
+print_r($random);
+$randomSlice = array_slice($random, 2, null, true);
+print_r($randomSlice);
